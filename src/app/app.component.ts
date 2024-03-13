b83100c1b0fabb2f71a58197f490eb809beaa890
Lufeltz <lufelt@proton.me>
2024-03-12 23:34:38 -0300

    Realizar a importação do PedidosComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PedidosComponent } from './pedidos/pedidos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PedidosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'LOL';
