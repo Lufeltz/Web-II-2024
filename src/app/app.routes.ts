aa0301f1c9991329507fb39637c363be58cb6145
marina-beppler <marina.beppler@ufpr.br>
2024-03-14 20:28:48 -0300

    Ajustes front na página de Autocadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login';
-import { CadastroComponent } from './pages/cadastro/cadastro/cadastro.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
