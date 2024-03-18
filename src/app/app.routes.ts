b59fe898db0cc7eaa09f69844a723cac206ccc63
marina-beppler <marina.beppler@ufpr.br>
2024-03-18 12:03:15 -0300

    Iniciada implementação do RF006

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login';
 import { CadastroComponent } from './pages/cadastro';
+import { PedidoComponent } from './pages/pedido/pedido.component';
+import { PedidosComponent } from '../app/pedidos/pedidos.component';
+
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'cadastro', component: CadastroComponent },
+    { path: 'pedido', component: PedidoComponent},
+    {path: 'pedidos', component: PedidosComponent}
     
 ];
