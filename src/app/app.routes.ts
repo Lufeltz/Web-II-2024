df18e82d0548b5a30095acf1625cbafb44c12da6
Matheus Henrique Miranda <henrique.matheus@ufpr.br>
2024-03-19 20:50:36 -0300

    Implementação da page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login';
 import { CadastroComponent } from './pages/cadastro';
-import { PedidoComponent } from './pages/pedido/pedido.component';
-import { PedidosComponent } from '../app/pedidos/pedidos.component';
+import { PedidosComponent } from './pages/pedidos';
+import { PedidoComponent } from './pages/pedido';
 import { ConsultaPedidoComponent } from './pages/consulta-pedido';
+import { PageNotFoundComponent } from './pages/page-not-found';
 
 export const routes: Routes = [
     { path: 'login', title: 'Login', component: LoginComponent },
     { path: 'cadastro', title: 'Cadastro', component: CadastroComponent },
-    { path: 'pedido', component: PedidoComponent },
-    { path: 'pedidos', component: PedidosComponent },
+    { path: 'pedidos', title:'Pedidos', component: PedidosComponent },
+    { path: 'pedido', title:'Pedido', component: PedidoComponent },
     { path: 'consulta-pedido', title: 'Consultar Pedido', component: ConsultaPedidoComponent },
     
     
     { path: '', redirectTo: 'login', pathMatch: 'full'}, // Rota padrão, se colocar o path do component em "redirectTo:", não precisará da tag/selector do seu component em app.component.html
-    //{ path: '**', component: PageNotFoundComponent },  // Rota para um component quando página der erro 404 (PageNotFoundComponent ainda não criado)
+    { path: '**', component: PageNotFoundComponent },  // Rota para um component quando página der erro 404
 ];
