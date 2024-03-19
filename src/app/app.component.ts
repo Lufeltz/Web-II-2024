13562d766e40233dee9f7f37bf0cb1eef3518c09
Matheus Henrique Miranda <henrique.matheus@ufpr.br>
2024-03-19 20:57:33 -0300

    Implementação inicial da menu navbar
    Implementei apenas o componente, mas não linkei a cada tela por enquanto.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,29 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './pages/login';
-import { PedidosComponent } from './pedidos/pedidos.component';
+import { CadastroComponent } from './pages/cadastro';
+import { PedidosComponent } from './pages/pedidos';
+import { PedidoComponent } from './pages/pedido';
 import { ConsultaPedidoComponent } from './pages/consulta-pedido';
+import { PageNotFoundComponent } from './pages/page-not-found';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    CommonModule, 
-    RouterOutlet, 
-    LoginComponent, 
-    PedidosComponent,
-    ConsultaPedidoComponent, 
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [
+      CommonModule,
+      RouterOutlet,
+      LoginComponent,
+      CadastroComponent,
+      PedidosComponent,
+      PedidoComponent,
+      ConsultaPedidoComponent,
+      PageNotFoundComponent,
+    ]
 })
+
 export class AppComponent {
   title = 'LOL';
 }
